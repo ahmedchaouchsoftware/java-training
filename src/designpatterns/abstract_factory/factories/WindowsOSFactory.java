8f08cdf76d56f44f93bcc1593bfedeb1a2820890
ahmedchaouchsoftware <ing.chaouch.ahmed@gmail.com>
2023-09-12 16:46:33 +0200

    Design Pattern Builder

--- src/designpatterns/abstract_factory/factories/WindowsOSFactory.java
+++ src/designpatterns/abstract_factory/factories/WindowsOSFactory.java
@@ -1,10 +1,8 @@
 package designpatterns.abstract_factory.factories;
 
 import designpatterns.abstract_factory.buttons.Button;
-import designpatterns.abstract_factory.buttons.MacOSButton;
 import designpatterns.abstract_factory.buttons.WindowsOSButton;
 import designpatterns.abstract_factory.checkboxes.CheckBox;
-import designpatterns.abstract_factory.checkboxes.MacOSCheckBox;
 import designpatterns.abstract_factory.checkboxes.WindowsOSCheckBox;
 
 public class WindowsOSFactory implements GUIFactory{
