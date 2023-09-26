dddff32115ad1dbd0c3e7291473618dbf5eb0df9
ahmedchaouchsoftware <ing.chaouch.ahmed@gmail.com>
2023-09-26 15:20:20 +0200

    Chapter 1 : write main method in java

--- src/Training.java
+++ src/Training.java
@@ -1,2 +1,7 @@
 public class Training {
+    public static void main(String[] args) {
+        System.out.print("Hello Ahmed CHAOUCH!");
+        //System.out.println(args[0]);
+        //System.out.println(args[1]);
+    }
 }
