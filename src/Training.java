fc4bf9cc7cb0289859b42c22b20d95402d604b84
ahmedchaouchsoftware <ing.chaouch.ahmed@gmail.com>
2023-09-26 15:22:16 +0200

    Chapter 1 : run main method with args

--- src/Training.java
+++ src/Training.java
@@ -1,7 +1,7 @@
 public class Training {
     public static void main(String[] args) {
-        System.out.print("Hello Ahmed CHAOUCH!");
-        //System.out.println(args[0]);
-        //System.out.println(args[1]);
+        //System.out.print("Hello Ahmed CHAOUCH!");
+        System.out.println(args[0]);
+        System.out.println(args[1]);
     }
 }
